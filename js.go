90eb54552164e671c9ee61b69fcc33210cc33c8a
Matt Jens Jensen <matt.jens.jensen+cs@gmail.com>
2020-03-20 12:39:56 -0700

    fixing xcash issue

--- js.go
+++ js.go
@@ -104,9 +104,9 @@ func snippet(js string, f func(n *cdp.Node) string, sel interface{}, n *cdp.Node
 func cashX(flatten bool) func(*cdp.Node) string {
 	return func(n *cdp.Node) string {
 		if flatten {
-			return fmt.Sprintf(`$x(%q)[0]`, n.FullXPath())
+			return fmt.Sprintf(`document.evaluate(%q, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue`, n.FullXPath())
 		}
-		return fmt.Sprintf(`$x(%q)`, n.FullXPath())
+		return fmt.Sprintf(`document.evaluate(%q, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)`, n.FullXPath())
 	}
 }
 
@@ -114,8 +114,8 @@ func cashX(flatten bool) func(*cdp.Node) string {
 func cashXNode(flatten bool) func(*cdp.Node) string {
 	return func(n *cdp.Node) string {
 		if flatten {
-			return fmt.Sprintf(`$x(%q)[0]`, n.FullXPath()+"/node()")
+			return fmt.Sprintf(`document.evaluate(%q, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue`, n.FullXPath()+"/node()")
 		}
-		return fmt.Sprintf(`$x(%q)`, n.FullXPath()+"/node()")
+		return fmt.Sprintf(`document.evaluate(%q, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)`, n.FullXPath()+"/node()")
 	}
 }
